453507dfbb4973a5682dcbd8908746acdb75ea9e
Samir Singh <samirsingh2000.ss@gmail.com>
2024-04-30 14:27:54 +0530

    Samir|Remote User audio and camera icon add and leave call functionality added|New

--- src/components/LiveVideo.js
+++ src/components/LiveVideo.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   LocalUser,
   RemoteUser,
@@ -16,16 +16,22 @@ import {
   BsFillCameraVideoFill,
   BsFillCameraVideoOffFill,
 } from "react-icons/bs";
+import { LuCamera, LuCameraOff } from "react-icons/lu";
+import { IoCall } from "react-icons/io5";
 import style from "./LiveVideo.module.css";
 
 const LiveVideo = () => {
   const appId = "001711e4544644e38215b13b01bfcd25";
   const { channelName } = useParams();
+  const navigate = useNavigate();
 
-  const activeConnection = true;
+  const [activeConnection, setActiveConnection] = useState(true);
 
   const [micOn, setMic] = useState(true);
   const [cameraOn, setCamera] = useState(true);
+
+  const [remoteMic, setRemoteMic] = useState(true);
+  const [remoteCamera, setRemoteCamera] = useState(true);
 
   const { localMicrophoneTrack } = useLocalMicrophoneTrack(micOn);
   const { localCameraTrack } = useLocalCameraTrack(cameraOn);
@@ -42,117 +48,97 @@ const LiveVideo = () => {
   usePublish([localMicrophoneTrack, localCameraTrack]);
 
   const remoteUsers = useRemoteUsers();
-  console.log("kjdshkj", remoteUsers);
   const { audioTracks } = useRemoteAudioTracks(remoteUsers);
 
   audioTracks.forEach((track) => track.play());
 
-  return (
-    <>
-      {/* <div id="remoteVideoGrid">
-        <span>Remote User</span>
-        {remoteUsers.map((user) => (
-          <div key={user.uid} className="remote-video-container">
-            <RemoteUser user={user} />
-          </div>
-        ))}
-      </div>
+  useEffect(() => {
+    if (remoteUsers && Array.isArray(remoteUsers)) {
+      setRemoteCamera(!remoteUsers[0]?._video_muted_);
+      setRemoteMic(!remoteUsers[0]?._audio_muted_);
+    }
+  }, [remoteUsers]);
 
-      <div id="localVideo">
-        <span>Local User</span>
-        <LocalUser
-          audioTrack={localMicrophoneTrack}
-          videoTrack={localCameraTrack}
-          cameraOn={cameraOn}
-          micOn={micOn}
-          playAudio={micOn}
-          playVideo={cameraOn}
-          className=""
-        />
-        <div>
-          <div id="controlsToolbar">
-            <div id="mediaControls">
-              <button className="btn" onClick={() => setMic((a) => !a)}>
-                {!micOn ? "MicOn" : "MicOff"}
-              </button>
-              <button className="btn" onClick={() => setCamera((a) => !a)}>
-                {!cameraOn ? "CameraOn" : "CameraOff"}
-              </button>
-            </div>
-            <button
-              id="endConnection"
+  console.log("kjdshkj", { remoteUsers, remoteCamera, remoteMic });
+
+  return (
+    <div className={style.main_div}>
+      <div className={style.userVideo}>
+        <h1>Local User</h1>
+        <div className={style.local_video_div}>
+          <LocalUser
+            audioTrack={localMicrophoneTrack}
+            videoTrack={localCameraTrack}
+            cameraOn={cameraOn}
+            micOn={micOn}
+            playAudio={micOn}
+            playVideo={cameraOn}
+            className={style.localUser_video}
+            style={{
+              height: "465px",
+            }}
+          />
+
+          <div className={style.controls_div}>
+            {micOn ? (
+              <FaMicrophone
+                className={style.mic_icon}
+                onClick={() => setMic((a) => !a)}
+              />
+            ) : (
+              <FaMicrophoneSlash
+                className={style.mic_icon}
+                onClick={() => setMic((a) => !a)}
+              />
+            )}
+
+            {cameraOn ? (
+              <BsFillCameraVideoFill
+                className={style.mic_icon}
+                onClick={() => setCamera((a) => !a)}
+              />
+            ) : (
+              <BsFillCameraVideoOffFill
+                className={style.mic_icon}
+                onClick={() => setCamera((a) => !a)}
+              />
+            )}
+
+            <IoCall
+              className={style.disconnect}
               onClick={() => {
                 setActiveConnection(false);
                 navigate("/");
               }}
-            >
-              {" "}
-              Disconnect
-            </button>
+            />
           </div>
         </div>
-      </div> */}
-
-      <div className={style.main_div}>
-        <div className={style.userVideo}>
-          <h1>Local User</h1>
-          <div className={style.local_video_div}>
-            <LocalUser
-              audioTrack={localMicrophoneTrack}
-              videoTrack={localCameraTrack}
-              cameraOn={cameraOn}
-              micOn={micOn}
-              playAudio={micOn}
-              playVideo={cameraOn}
+      </div>
+      <div className={style.userVideo}>
+        <h1>Remote User</h1>
+        <div className={style.local_video_div}>
+          {remoteUsers[0] && (
+            <RemoteUser
+              user={remoteUsers[0]}
               className={style.localUser_video}
               style={{
                 height: "465px",
               }}
             />
-
-            <div className={style.controls_div}>
-              {micOn ? (
-                <FaMicrophone
-                  className={style.mic_icon}
-                  onClick={() => setMic((a) => !a)}
-                />
-              ) : (
-                <FaMicrophoneSlash
-                  className={style.mic_icon}
-                  onClick={() => setMic((a) => !a)}
-                />
-              )}
-
-              {cameraOn ? (
-                <BsFillCameraVideoFill
-                  className={style.mic_icon}
-                  onClick={() => setCamera((a) => !a)}
-                />
-              ) : (
-                <BsFillCameraVideoOffFill
-                  className={style.mic_icon}
-                  onClick={() => setCamera((a) => !a)}
-                />
-              )}
-            </div>
-          </div>
-        </div>
-        <div className={style.userVideo}>
-          <h1>Remote User</h1>
-          <div className={style.video_div}>
-            {remoteUsers[0] && (
-              <RemoteUser
-                user={remoteUsers[0]}
-                className={style.localUser_video}
-                style={{
-                  height: "465px",
-                }}
-              />
-            )}
-          </div>
+          )}
+          {remoteCamera ? (
+            <LuCamera className={style.remote_camera} />
+          ) : (
+            <LuCameraOff className={style.remote_camera} />
+          )}
+          {remoteMic ? (
+            <FaMicrophone className={style.remote_mic} />
+          ) : (
+            <FaMicrophoneSlash className={style.remote_mic} />
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
