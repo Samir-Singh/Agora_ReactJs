7f6a46fe8951543d7f78f82a8ddd4150ab7e69b9
Samir Singh <samirsingh2000.ss@gmail.com>
2024-05-02 12:17:13 +0530

    Showing mute audio and video icon on remote side added

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   LocalVideoTrack,
   RemoteUser,
@@ -8,6 +8,8 @@ import {
   useRemoteUsers,
 } from "agora-rtc-react";
 import { useParams } from "react-router-dom";
+import { IoMdMicOff } from "react-icons/io";
+import { BiSolidCameraOff } from "react-icons/bi";
 
 const Videos = ({
   isLoadingMic,
@@ -15,8 +17,13 @@ const Videos = ({
   localMicrophoneTrack,
   localCameraTrack,
 }) => {
+  const [remoteMute, setRemoteMute] = useState({
+    muteVideo: false,
+    muteAudio: false,
+  });
   const params = useParams();
   const remoteUsers = useRemoteUsers();
+
   const { audioTracks } = useRemoteAudioTracks(remoteUsers);
 
   usePublish([localMicrophoneTrack, localCameraTrack]);
@@ -30,20 +37,36 @@ const Videos = ({
 
   const deviceLoading = isLoadingMic || isLoadingCam;
 
+  useEffect(() => {
+    if (remoteUsers) {
+      setRemoteMute({
+        muteVideo: remoteUsers[0]?._video_muted_,
+        muteAudio: remoteUsers[0]?._audio_muted_,
+      });
+    }
+  }, [remoteUsers]);
+
   if (deviceLoading)
     return (
       <div className="flex flex-col items-center pt-40">Loading devices...</div>
     );
 
   return (
-    <div className="flex flex-row justify-between w-full h-screen p-1">
-      <div className={`flex flex-row gap-1 flex-1`}>
+    <div className="flex flex-row justify-between w-full h-screen">
+      <div className={`flex flex-row gap-1 flex-1 relative`}>
         <LocalVideoTrack
           track={localCameraTrack}
           play={true}
           className="w-full h-full"
         />
         {remoteUsers && <RemoteUser user={remoteUsers[0]} />}
+
+        {remoteMute?.muteAudio && (
+          <IoMdMicOff className="absolute top-5 right-5 text-xl text-white" />
+        )}
+        {remoteMute?.muteVideo && (
+          <BiSolidCameraOff className="absolute bottom-5 right-5 text-xl text-white" />
+        )}
       </div>
     </div>
   );
